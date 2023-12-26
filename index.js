b23ca0657335a8e6f5c8f5dc9bda5415795b46d9
Tristan Salinas <salinas.tristan@gmail.com>
2023-12-26 15:21:37 +0100

    added ui to the game

--- index.js
+++ index.js
@@ -3,47 +3,28 @@ let computerWins = 0;
 let playerWins = 0;
 
 
-function getComputerChoice () {
-    return choice [Math.floor(Math.random() * choice.length)];
-}
-
-function getPlayerChoice () {
-    let playerChoice = prompt("Score : " + playerWins +"/"+ computerWins + "\nChose between Rock, Paper or scissors");
-    
-
-    if (choice.includes(playerChoice.toLowerCase())){
-        return playerChoice.toLowerCase();
-    } else {
-        alert (playerChoice + " is not a valid answer!");
-        return getPlayerChoice();
-    }   
-}
-
-
-
-
-function playRound () {
+const playRound = function(playerChoice) {
     let computerChoice = getComputerChoice();
-    let playerChoice = getPlayerChoice();
-
-    console.log(computerChoice + " " + playerChoice);
+    
+    
+    
 
     switch (true) {
         //en cas d'égalité :
         case (playerChoice === computerChoice) :
-            alert ("Nobody won, you both chosed : "+ playerChoice + "! Let's remake the round");
+            uiText.innerText = "Score : " + playerWins +"/"+ computerWins +"\n Nobody won, you both chosed : "+ playerChoice + "! Let's remake the round";
         return playRound();
         //sinon
         case (playerChoice === "rock") : 
             switch (true) {
                 case (computerChoice === "paper") :
                     computerWins += 1;
-                    alert ("Score : " + playerWins +"/"+ computerWins + "\nyou lose, computer chose paper");
+                    uiText.innerText ="Score : " + playerWins +"/"+ computerWins + "\nyou lose, computer chose paper";
                 break;
 
                 case (computerChoice === "scissors") :
                     playerWins += 1;
-                    alert ("Score : " + playerWins +"/"+ computerWins + "\nyou win, computer chose scissors");
+                    uiText.innerText ="Score : " + playerWins +"/"+ computerWins + "\nyou win, computer chose scissors";
                 break;
             }
         break;
@@ -52,11 +33,11 @@ function playRound () {
             switch (true) {
                 case (computerChoice === "rock") :
                     playerWins += 1;
-                    alert ("Score : " + playerWins +"/"+ computerWins + "\nyou win, computer chose rock");
+                    uiText.innerText ="Score : " + playerWins +"/"+ computerWins + "\nyou win, computer chose rock";
                 break;
                 case (computerChoice === "scissors") :
                     computerWins += 1;
-                    alert ("Score : " + playerWins +"/"+ computerWins + "\nyou lose, computer chose scissors");
+                    uiText.innerText ="Score : " + playerWins +"/"+ computerWins + "\nyou lose, computer chose scissors";
                 break;
             }
         break;
@@ -65,29 +46,58 @@ function playRound () {
             switch (true) {
                 case (computerChoice === "rock") :
                     computerWins += 1;
-                    alert ("Score : " + playerWins +"/"+ computerWins + "\nyou lose, computer chose rock");
+                    uiText.innerText ="Score : " + playerWins +"/"+ computerWins + "\nyou lose, computer chose rock";
                 break;
                 case (computerChoice === "paper") :
                     playerWins += 1;
-                    alert ("Score : " + playerWins +"/"+ computerWins + "\nyou win, computer chose paper");
+                    uiText.innerText ="Score : " + playerWins +"/"+ computerWins + "\nyou win, computer chose paper";
                 break;
             }
         break;      
     }
 }
 
-function game() {
 
 
-    nbrOfRounds = parseInt(prompt("Chose a number of rounds : "));
+const gameBox = document.querySelector('#gameBox');
+const uiText = document.createElement('p');
 
-    for (i=0; i< nbrOfRounds; i++) {
-        playRound();
-    }
+const rockButton = document.createElement('button');
+rockButton.textContent = "Rock" ;
+rockButton.addEventListener("click", function() {
+    playRound("rock");
+});
 
-    alert ("launching another game ?");
-    return game();
+const paperButton = document.createElement('button');
+paperButton.textContent = "Paper" ;
+paperButton.addEventListener("click", function() {
+    playRound("paper");
+});
 
+const scissorsButton = document.createElement('button');
+scissorsButton.textContent = "Scissor" ;
+scissorsButton.addEventListener("click", function() {
+    playRound("scissors");
+    
+});
+
+
+
+function getComputerChoice () {
+    return choice [Math.floor(Math.random() * choice.length)];
 }
 
-game();
+
+
+
+
+
+
+gameBox.append(uiText);
+
+gameBox.append(rockButton);
+gameBox.append(paperButton);
+gameBox.append(scissorsButton);
+
+
+
